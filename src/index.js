b8322ff09d4c73530c64f4a1fec9f6d0d255f1e4
jeffreysham <jeffreytys@hotmail.com>
2017-10-12 00:45:33 -0400

    submitted for certification

--- src/index.js
+++ src/index.js
@@ -1,50 +1,48 @@
 'use strict';
 var Alexa = require("alexa-sdk");
-var AWS = require("aws-sdk");
-var promisify = require("es6-promisify");
+var fs = require("fs");
 var appId = 'amzn1.ask.skill.16258e7c-8576-410f-bd1b-c3f98b81d460';
 
-var userMap = {}; // Map of user id to madlib story
-
-var docClient = new AWS.DynamoDB.DocumentClient();
-var dbGet = promisify(docClient.get, docClient);
+var userMap = {}; // Map of user id to madlib story index
+var userDataMap = {}; // Map of user id to madlib story data
+var dataJson;
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = appId;
 
-    if (!(event.session.user.userId in userMap)) {
-        userMap[event.session.user.userId] = 1;
+    if (!(event.context.System.user.userId in userMap)) {
+        userMap[event.context.System.user.userId] = 0;
     }
-    alexa.registerHandlers(newSessionHandlers, startGameHandlers);
+
+    readWholeFile();
+
+    alexa.registerHandlers(newSessionHandlers, startGameHandlers, promptGameHandlers);
     alexa.execute();
 };
 
 var states = {
-    GUESSMODE: '_GUESSMODE', // User is prompted to give parts of speech.
+    PROMPTMODE: '_PROMPTMODE', // User is prompted to give parts of speech.
     STARTMODE: '_STARTMODE'  // Prompt the user to start or restart the game.
 };
 
-var read = function(id) {
-    var params = {
-        TableName: "MadLibs",
-        Key: {
-            id: id
-        }
-    };
-    
-    dbGet(params)
-      .then(data => {
-        console.log('Get item succeeded', data);
-      })
-      .catch(err => console.log(err));
+var readWholeFile = function() {
+    var allData = fs.readFileSync("data.json");
+    dataJson = JSON.parse(allData);
 }
 
+var initializeUserData = function(index, event) {
+    if (index >= dataJson.length) {
+        index = 0;
+        userMap[event.context.System.user.userId] = 0;
+    }
+
+    userDataMap[event.context.System.user.userId] = dataJson[index];
+}
 
 var newSessionHandlers = {
     'NewSession': function() {
         this.handler.state = states.STARTMODE;
-        read(1);
         this.emit(':ask', 'Welcome to Mad Libs for Kids. Would you like to play?',
             'Say yes to start the game or no to quit.');
     },
@@ -64,14 +62,22 @@ var newSessionHandlers = {
 
 
 var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
+    'NewSession': function () {
+        this.emit('NewSession'); // Uses the handler in newSessionHandlers
+    },
     'AMAZON.HelpIntent': function() {
         var message = 'I will first prompt you for a few parts of speech. Afterwards, I will read your newly ' +
             'created story. Do you want to start?';
         this.emit(':ask', message, message);
     },
     'AMAZON.YesIntent': function() {
-        read(1);
-        this.emit(':ask', 'Great! ' + 'Try saying a number to start the game.', 'Try saying a number.');
+        initializeUserData(userMap[this.event.context.System.user.userId], this.event);
+        this.handler.state = states.PROMPTMODE;
+
+        var partOfSpeech = userDataMap[this.event.context.System.user.userId].partsOfSpeech[0];
+        partOfSpeech = partOfSpeech.replace(/[<>]/g, "");
+        var article = getArticle(partOfSpeech);
+        this.emit(':ask', 'Great! Try saying ' + article + ' ' + partOfSpeech + ' to start the game.', 'Try saying ' + article + ' ' + partOfSpeech + '.');
     },
     'AMAZON.NoIntent': function() {
         console.log("NOINTENT");
@@ -96,135 +102,111 @@ var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
     }
 });
 
-
-
-
-
-
-/*
-
-var newSessionHandlers = {
-    'NewSession': function() {
-        if(Object.keys(this.attributes).length === 0) {
-            this.attributes['endedSessionCount'] = 0;
-            this.attributes['gamesPlayed'] = 0;
-        }
-        this.handler.state = states.STARTMODE;
-        this.emit(':ask', 'Welcome to Mad Libs for Kids. You have played '
-            + this.attributes['gamesPlayed'].toString() + ' times. would you like to play?',
-            'Say yes to start the game or no to quit.');
-    },
-    "AMAZON.StopIntent": function() {
-      this.emit(':tell', "Goodbye!");  
-    },
-    "AMAZON.CancelIntent": function() {
-      this.emit(':tell', "Goodbye!");  
-    },
-    'SessionEndedRequest': function () {
-        console.log('session ended!');
-        //this.attributes['endedSessionCount'] += 1;
-        this.emit(":tell", "Goodbye!");
-    }
-};
-
-var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
-    'NewSession': function () {
-        this.emit('NewSession'); // Uses the handler in newSessionHandlers
-    },
-    'AMAZON.HelpIntent': function() {
-        var message = 'I will think of a number between zero and one hundred, try to guess and I will tell you if it' +
-            ' is higher or lower. Do you want to start the game?';
-        this.emit(':ask', message, message);
-    },
-    'AMAZON.YesIntent': function() {
-        this.attributes["guessNumber"] = Math.floor(Math.random() * 100);
-        this.handler.state = states.GUESSMODE;
-        this.emit(':ask', 'Great! ' + 'Try saying a number to start the game.', 'Try saying a number.');
-    },
-    'AMAZON.NoIntent': function() {
-        console.log("NOINTENT");
-        this.emit(':tell', 'Ok, see you next time!');
-    },
-    "AMAZON.StopIntent": function() {
-      console.log("STOPINTENT");
-      this.emit(':tell', "Goodbye!");  
-    },
-    "AMAZON.CancelIntent": function() {
-      console.log("CANCELINTENT");
-      this.emit(':tell', "Goodbye!");  
-    },
-    'SessionEndedRequest': function () {
-        console.log("SESSIONENDEDREQUEST");
-        //this.attributes['endedSessionCount'] += 1;
-        this.emit(':tell', "Goodbye!");
-    },
-    'Unhandled': function() {
-        console.log("UNHANDLED");
-        var message = 'Say yes to continue, or no to end the game.';
-        this.emit(':ask', message, message);
-    }
-});
-
-var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
+var promptGameHandlers = Alexa.CreateStateHandler(states.PROMPTMODE, {
     'NewSession': function () {
         this.handler.state = '';
-        this.emitWithState('NewSession'); // Equivalent to the Start Mode NewSession handler
+        this.emit('NewSession'); // Uses the handler in newSessionHandlers
     },
-    'NumberGuessIntent': function() {
-        var guessNum = parseInt(this.event.request.intent.slots.number.value);
-        var targetNum = this.attributes["guessNumber"];
-        console.log('user guessed: ' + guessNum);
+    'PartOfSpeechIntent': function() {
+        var partsOfSpeech = userDataMap[this.event.context.System.user.userId].partsOfSpeech;
+        var content = userDataMap[this.event.context.System.user.userId].content;
+        var title = userDataMap[this.event.context.System.user.userId].title;
 
-        if(guessNum > targetNum){
-            this.emit('TooHigh', guessNum);
-        } else if( guessNum < targetNum){
-            this.emit('TooLow', guessNum);
-        } else if (guessNum === targetNum){
-            // With a callback, use the arrow function to preserve the correct 'this' context
-            this.emit('JustRight', () => {
-                this.emit(':ask', guessNum.toString() + 'is correct! Would you like to play a new game?',
-                'Say yes to start a new game, or no to end the game.');
-        })
+        var partOfSpeech = partsOfSpeech[0];
+        var userInput = this.event.request.intent.slots.partOfSpeech.value;
+        content = content.replace(partOfSpeech, userInput);
+        partsOfSpeech.shift();
+        userDataMap[this.event.context.System.user.userId].content = content;
+        userDataMap[this.event.context.System.user.userId].partsOfSpeech = partsOfSpeech;
+
+        if (partsOfSpeech.length > 0) {
+            var partOfSpeech = userDataMap[this.event.context.System.user.userId].partsOfSpeech[0];
+            partOfSpeech = partOfSpeech.replace(/[<>]/g, "");
+            var article = getArticle(partOfSpeech);
+            var excitingResponse = getExcitingResponse();
+            this.emit(':ask', excitingResponse + ' Say ' + article + ' ' + partOfSpeech, 'Try saying ' + article + ' ' + partOfSpeech + '.');
         } else {
-            this.emit('NotANum');
+            var message = 'Awesome! Now we can listen to your newly created story. The title is ';
+            message += title + '. ' + content;
+            message += ' Would you like to do another mad lib?';
+            this.handler.state = states.STARTMODE;
+            userMap[this.event.context.System.user.userId]++;
+            this.emit(':ask', message, 'Would you like to do another mad lib?');
         }
     },
     'AMAZON.HelpIntent': function() {
-        this.emit(':ask', 'I am thinking of a number between zero and one hundred, try to guess and I will tell you' +
-            ' if it is higher or lower.', 'Try saying a number.');
+        var partsOfSpeech = userDataMap[this.event.context.System.user.userId].partsOfSpeech;
+
+        if (partsOfSpeech.length > 0) {
+            var partOfSpeech = partsOfSpeech[0];
+            partOfSpeech = partOfSpeech.replace(/[<>]/g, "");
+            var article = getArticle(partOfSpeech);
+            this.emit(':ask', 'Please say ' + article + ' ' + partOfSpeech, 'Try saying ' + article + ' ' + partOfSpeech + '.');
+        } else {
+            this.emit(':ask', 'Would you like to do another mad lib?', 'Would you like to do another mad lib?');
+        }
     },
     "AMAZON.StopIntent": function() {
-        console.log("STOPINTENT");
+      console.log("STOPINTENT");
       this.emit(':tell', "Goodbye!");  
     },
     "AMAZON.CancelIntent": function() {
-        console.log("CANCELINTENT");
+      console.log("CANCELINTENT");
+      this.emit(':tell', "Goodbye!");  
     },
     'SessionEndedRequest': function () {
         console.log("SESSIONENDEDREQUEST");
-        this.attributes['endedSessionCount'] += 1;
         this.emit(':tell', "Goodbye!");
     },
     'Unhandled': function() {
-        console.log("UNHANDLED");
-        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+        var partsOfSpeech = userDataMap[this.event.context.System.user.userId].partsOfSpeech;
+        if (partsOfSpeech.length > 0) {
+            var partOfSpeech = partsOfSpeech[0];
+            partOfSpeech = partOfSpeech.replace(/[<>]/g, "");
+            var article = getArticle(partOfSpeech);
+            this.emit(':ask', 'Please say ' + article + ' ' + partOfSpeech, 'Try saying ' + article + ' ' + partOfSpeech + '.');
+        } else {
+            this.emit(':ask', 'Would you like to do another mad lib?', 'Would you like to do another mad lib?');
+        }
     }
 });
 
-// These handlers are not bound to a state
-var guessAttemptHandlers = {
-    'TooHigh': function(val) {
-        this.emit(':ask', val.toString() + ' is too high.', 'Try saying a smaller number.');
-    },
-    'TooLow': function(val) {
-        this.emit(':ask', val.toString() + ' is too low.', 'Try saying a larger number.');
-    },
-    'JustRight': function(callback) {
-        this.handler.state = states.STARTMODE;
-        this.attributes['gamesPlayed']++;
-        callback();
-    },
-    'NotANum': function() {
-        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+var getArticle = function(word) {
+    if (word.charAt(0) == 'a' ||
+            word.charAt(0) == 'e' ||
+            word.charAt(0) == 'i' ||
+            word.charAt(0) == 'o' ||
+            word.charAt(0) == 'u') {
+        return 'an';
+    } else {
+        return 'a';
     }
-};*/
+}
+
+var getExcitingResponse = function() {
+    var number = Math.floor((Math.random() * 10));
+    switch (number) {
+        case 0:
+            return "Thanks.";
+        case 1:
+            return "Great!";
+        case 2:
+            return "Good Job.";
+        case 3:
+            return "Nice one!";
+        case 4:
+            return "Keep it up.";
+        case 5:
+            return "Cool.";
+        case 6:
+            return "Wow! I didn't think of that one.";
+        case 7:
+            return "Not bad.";
+        case 8:
+            return "Interesting.";
+        case 9:
+            return "Neato.";
+        default:
+            return "";
+    }
+}
